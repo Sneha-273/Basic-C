6b55fdf1201d04e3761702d69fac9e796dc2ac0c
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-01-05 10:02:28 +0530

    Add files via upload

---
+++ Bitwise AND.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m;
+    printf("First Decimal number=");
+    scanf("%d",&n);
+    printf("second Decimal number=");
+    scanf("%d",&m);
+    printf("after AND operation=%d",n&m);   
+    return 0;
+}
