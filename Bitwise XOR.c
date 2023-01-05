fb92768739dc7f769302662eb29f5aa87cc5fb25
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-01-05 10:03:17 +0530

    Add files via upload

---
+++ Bitwise XOR.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int n1,n2;
+    printf("First Decimal number=");
+    scanf("%d",&n1);
+    printf("Second Decimal number=");
+    scanf("%d",&n2);
+    printf("after EX-OR operation=%d",n1^n2);
+    return 0;
+}
