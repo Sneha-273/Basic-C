881f30c76afb8fea48ba30c905a0a6b0a0e76c68
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-01-05 10:03:56 +0530

    Add files via upload

---
+++ Bitwise negation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int n1;
+    printf("Find negation.");
+    printf("\nEnter Decimal number=");
+    scanf("%d",&n1);
+    printf("After negation Operation=%d",~n1);
+    return 0;
+}
